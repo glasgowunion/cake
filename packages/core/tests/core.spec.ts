04b3d094558f47e08e96900ffb1ceffd79ef345b
Jamie Laux <jamie@bleeppurple.com>
2021-04-04 17:42:43 +0100

    fix: some failing tests - used to test gh actions

--- packages/core/tests/core.spec.ts
+++ packages/core/tests/core.spec.ts
@@ -14,14 +14,13 @@ describe('@core > creating a invalid cake', () => {
 
   it('should throw an error when specification is not met - invoked with an zero id', () => {
     const newCake = () => Cake(mocks.core.cake.invalidZeroID);
-    expect(newCake).toThrowError('help');
+    expect(newCake).toThrowError(`"id" must be greater than or equal to 1`);
   });
 });
 
 describe('@core > creating a a valid cake', () => {
   it('should create a cake when intanstiated witha valid object', () => {
-    // creates cake with object that satisfies type constraints but not schema
-    const newCake = () => Cake(mocks.core.cake.valid);
-    expect(newCake).toThrow();
+    const cake = Cake(mocks.core.cake.valid);
+    expect(cake).toEqual(mocks.core.cake.valid);
   });
 });
