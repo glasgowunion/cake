e83347a929ec87a6932f6c4bfa6e128c7e22293a
Jamie Laux <gu@bleeppurple.com>
2021-04-12 16:06:17 +0100

    fix: remove gradle build folder from jest paths

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverage: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['build'],
   testMatch: ['**/tests/**/*.[jt]s?(x)'],
   testResultsProcessor: 'jest-sonar-reporter',
   coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
