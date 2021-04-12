81bda90d930e2763660b95e22b8603a3d4753adb
Jamie Laux <gu@bleeppurple.com>
2021-04-12 12:27:33 +0100

    add: contracts for lambda queries and responses

---
+++ src/Contract.Lambda.Responses.ts
@@ -0,0 +1,19 @@
+export interface LambdaAnyResponse {
+  statusCode: number;
+  body: string;
+}
+
+export interface LambdaFailResponse {
+  statusCode: 500;
+  body: string;
+}
+
+export interface LambdaNotFoundResponse {
+  statusCode: 404;
+  body: string;
+}
+
+export interface LambdaSuccessResponse {
+  statusCode: 200;
+  body: string;
+}
